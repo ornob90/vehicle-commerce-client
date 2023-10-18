e17f9807c62642e4f507c1c15e4c1125c7df0d5a
ornob90 <oornob49@gmail.com>
2023-10-18 10:10:13 +0600

    banner responsive complete

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -4,22 +4,22 @@ import Button from "../Shared/Button";
 
 const Banner = () => {
   return (
-    <Container className="grid grid-cols-1 md:grid-cols-5 min-h-[300px] pt-14 bg-gradient-to-r from-white to-[#FFEDEA] ">
-      <div className=" md:col-span-2 flex flex-col justify-center gap-4 w-[90%] mx-auto">
-        <h3 className="text-xl font-semibold ">
+    <Container className="grid grid-cols-1 md:grid-cols-5 min-h-[300px] pt-14 bg-gradient-to-r from-white to-[#FFEDEA] gap-8 md:gap-0">
+      <div className=" md:col-span-2 flex flex-col justify-center  gap-4 w-[90%] mx-auto">
+        <h3 className="text-md sm:text-lg lg:text-xl font-semibold ">
           Discover Excellence in Driving
         </h3>
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold">
           Explore <span className="text-[#FF4D30]">Luxury</span> and{" "}
           <span className="text-[#FF4D30]">Performance</span>
         </h1>
-        <p className="text-[#706F7B]">
+        <p className="text-[#706F7B] text-sm lg:text-base">
           Find your perfect ride from our wide range of top-notch vehicles.
           Experience the pinnacle of engineering, style, and comfort
         </p>
 
         <div>
-          <Button className="bg-[#FF4D30] text-white px-8 py-3 font-medium rounded-sm">
+          <Button className="bg-[#FF4D30] text-white px-6 lg:px-8 py-2 lg:py-3 font-medium rounded-sm">
             View Brands
           </Button>
         </div>
