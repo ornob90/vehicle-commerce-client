f1d760ea256463e59873391fd11b8d8d60ccacef
ornob90 <oornob49@gmail.com>
2023-10-19 06:50:59 +0600

    setting up firebase and finishing the login setup

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useContext, useRef, useState } from "react";
 import Container from "../../components/Shared/Container";
 import { FcGoogle } from "react-icons/fc";
 import Button from "../../components/Shared/Button";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import AuthContext from "../../Context/AuthContext";
+import Swal from "sweetalert2";
 
 const Login = () => {
+  const { signInMethod, googleSignInMethod } = useContext(AuthContext);
+  const emailRef = useRef();
+  const [showPass, setShowPass] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const { state } = useLocation();
   const navigate = useNavigate();
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    e.target.email.value = "";
+    e.target.password.value = "";
+
+    signInMethod(email, password)
+      .then((res) => {
+        setErrorMsg("");
+
+        Swal.fire({
+          icon: "success",
+          title: "You have successfully logged in",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+
+        if (state) {
+          navigate(state);
+        } else {
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        setErrorMsg(err.message);
+        console.log(err.message);
+      });
+  };
+
+  const handleGoogleSignIn = () => {
+    googleSignInMethod()
+      .then((res) => {
+        setErrorMsg("");
+        if (state) {
+          navigate(state);
+        } else {
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        setErrorMsg(err.message);
+        console.log(err);
+      });
+  };
+
   return (
     <Container className="min-h-screen w-full bg-banner relative min-w-screen  flex justify-center items-center">
       <div className="backdrop-blur-md	 p-4  flex flex-col gap-4 rounded-xl   w-[70%] sm:w-[50%] md:w-[40%] lg:w-[26%] border border-white text-white">
         <p className="text-center text-white text-3xl font-[900]">Login</p>
-        <form
-          className="flex flex-col gap-4"
-          //  onSubmit={handleSignIn}
-        >
+        <form className="flex flex-col gap-4" onSubmit={handleSignIn}>
           <input
             required
             className=" pl-5 py-3 rounded-lg text-[10px] md:text-sm focus:outline-none bg-transparent border border-gray-600"
@@ -28,6 +79,9 @@ const Login = () => {
             placeholder="Enter Password"
             name="password"
           />
+
+          {errorMsg && <p className="text-red-600 text-sm">{errorMsg}</p>}
+
           <p className="w-max text-[12px]  active:scale-95 duration-300 cursor-pointer">
             Forget Password
           </p>
@@ -56,7 +110,7 @@ const Login = () => {
             <AiFillTwitterCircle className="text-blue-600 text-4xl cursor-pointer " />
           </div> */}
         <div
-          // onClick={handleGoogleSignIn}
+          onClick={handleGoogleSignIn}
           className="flex items-center bg-blue-600 text-white rounded-lg justify-center text-sm md:text-base cursor-pointer active:scale-95 duration-[.35s]"
         >
           <div className="h-full py-1 px-2 rounded-l-lg flex justify-center items-center gap-4">
